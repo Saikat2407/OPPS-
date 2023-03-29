cc20eacb7958884c92d2f1e09bf1dee74509de09
Saikat <s.nayek2407@gmail.com>
2023-03-29 18:12:26 +0530

    code Added

--- src/Saikat/Introduction/finalandfinalize.java
+++ src/Saikat/Introduction/finalandfinalize.java
@@ -4,12 +4,15 @@ public class finalandfinalize {
     public static void main(String[] args) {
 
         final A obj = new A("Saikat Nayek");
+        final int num = 12;  //final keyword doesn't let you manipulate once you assigned.
         System.out.println(obj.name);
+        System.out.println(obj.num);
     }
 }
 
 class A {
     final int num = 10;
+
     String name;
 
     public A(String name) {
