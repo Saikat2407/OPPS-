153e8b38020f73a8e6393f6f19bf98ee754b1ea6
Saikat <s.nayek2407@gmail.com>
2023-04-04 19:56:46 +0530

    inheritance code

--- src/Inheritance/main.java
+++ src/Inheritance/main.java
@@ -7,6 +7,9 @@ public class main {
         BoxWeight box2 = new BoxWeight(23.0);
         System.out.println(box1.high + " " + box2.weight);
 
+        BoxWeight box3 = new BoxWeight(12.9,99.0,89.0,4.9);
+        System.out.println(box3.high + " " + box3.wid+ " " + box3.len + " " + box2.weight);
+
 
 //        System.out.println(box1.len + " " + box1.wid + " " + box1.high);
     }
